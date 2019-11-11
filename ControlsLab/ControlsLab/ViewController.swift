3242dd0f1d38eadcdbd61811185257888c8ba044
Adán Diaz <adamdiaz@pursuit.org>
2019-11-11 17:50:57 -0500

    complete 90% of lab, in need of assistance

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -11,52 +11,54 @@ class ViewController: UIViewController {
     
     // MARK: Stepper
     
-    @IBOutlet weak var stepperLabel: UILabel!
     
     @IBOutlet weak var stepperCont: UIStepper!
     
     // MARK: UIImage
     
     @IBOutlet weak var cardNum: UILabel!
-    
-    var cardNumChange: Int = 1
+
     
     @IBOutlet weak var cardSym: UIImageView!
     
-    var suitSegment: Int = 0 {
+    var valueNum: Double = 2 {
         didSet {
-            switch segmentedControl.selectedSegmentIndex {
-            case 0:
-                cardSym.image = UIImage(systemName: "suit.diamond.fill")
-                //cardSym.setImage(UIImage(systemName: "pencil.slash"), for: .normal))
-            case 1:
-               cardSym.image = UIImage(systemName: "suit.heart.fill")
-            case 2:
-                cardSym.image = UIImage(systemName: "suit.club.fill")
-            case 3:
-                cardSym.image = UIImage(systemName: "suit.spade.fill")
-            default:
-                cardSym.image = UIImage(systemName: "pencil.slash")
-            }
+            cardNum.text = "\(stepperCont.value)"
         }
     }
     
-    func configureStepper() {
-        stepperCont.minimumValue = 1
-        stepperCont.maximumValue = 13
-        stepperCont.stepValue = 1
-
-        stepperCont.value = 4
-    }
+        var suitSegment: Int = 0 {
+            didSet {
+                switch segmentedControl.selectedSegmentIndex {
+                case 0:
+                    cardSym.image = UIImage(systemName: "suit.diamond.fill")
+                case 1:
+                   cardSym.image = UIImage(systemName: "suit.heart.fill")
+                case 2:
+                    cardSym.image = UIImage(systemName: "suit.club.fill")
+                case 3:
+                    cardSym.image = UIImage(systemName: "suit.spade.fill")
+                default:
+                    cardSym.image = UIImage(systemName: "suit.heart")
+                }
+            }
+        }
+    
+ 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cardNum.text = "A"
     }
     
     
-    
+    func configureStepper() {
+        stepperCont.minimumValue = 2.0
+        stepperCont.maximumValue = 13.0
+        stepperCont.stepValue = 2.0
+        stepperCont.value = 4.0
+     }
     
     
     @IBAction func segmentSuits(_ sender: UISegmentedControl) {
@@ -67,6 +69,22 @@ class ViewController: UIViewController {
     
     
     
+    @IBAction func cardCount(_ sender: UIStepper) {
+        cardNum.text = String(format: "%0.f", stepperCont.value)
+        if stepperCont.value > 10.0 {
+            switch stepperCont.value {
+            case 11.0:
+                cardNum.text = "J"
+            case 12.0:
+                cardNum.text = "Q"
+            case 13.0:
+                cardNum.text = "K"
+            default:
+                cardNum.text = "JK"
+            }
+        }
+
+    }
     
     
     
