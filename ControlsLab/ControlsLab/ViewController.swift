85455486d9c9992f66c8b634c017fb8b88cd0f97
Adán Diaz <adamdiaz@pursuit.org>
2019-11-11 16:00:42 -0500

    started creating y connections.

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -2,6 +2,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Segmented
+    
+    
+    @IBOutlet weak var segmentLabel: UILabel!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    // MARK: Stepper
+    
+    @IBOutlet weak var stepperLabel: UILabel!
+    
+    @IBOutlet weak var stepperControl: UIStepper!
+    
+    // MARK: UIImage
+    
+    @IBOutlet weak var cardNum: UILabel!
+    
+    @IBOutlet weak var cardSym: UIImageView!
+    
+    var value 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
